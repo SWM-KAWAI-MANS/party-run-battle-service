9b1c43c2c4ce72a551ec1ad62f3f89f52b34e2b2
seongwoo <seongwoo0513@uos.ac.kr>
2023-06-25 11:13:26 +0900

    project setting

---
+++ src/main/java/online/partyrun/partyrunbattleservice/PartyRunBattleServiceApplication.java
@@ -0,0 +1,13 @@
+package online.partyrun.partyrunbattleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyRunBattleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyRunBattleServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/partyrun/partyrunbattleservice/PartyRunBattleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package online.partyrun.partyrunbattleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyRunBattleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
