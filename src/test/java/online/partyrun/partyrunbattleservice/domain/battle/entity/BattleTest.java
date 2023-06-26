30650aacb08a97d9abe05c9bb3dd39e8fbfb9f3c
seongwoo <seongwoo0513@uos.ac.kr>
2023-06-26 20:19:25 +0900

    refactor : Runner 객체 선언으로 재사용 #PAR-106

--- src/test/java/online/partyrun/partyrunbattleservice/domain/battle/entity/BattleTest.java
+++ src/test/java/online/partyrun/partyrunbattleservice/domain/battle/entity/BattleTest.java
@@ -1,24 +1,26 @@
 package online.partyrun.partyrunbattleservice.domain.battle.entity;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import online.partyrun.partyrunbattleservice.domain.battle.exception.InvalidNumberOfBattleRunnerException;
 import online.partyrun.partyrunbattleservice.domain.runner.entuty.Runner;
-
 import org.junit.jupiter.api.*;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @DisplayName("Battle")
 class BattleTest {
+
+    Runner 박성우 = new Runner("1", "박성우");
+    Runner 노준혁 = new Runner("2", "노준혁");
+    Runner 박현준 = new Runner("3", "박현준");
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
     class 러너들이_주어졌을_때 {
 
-        List<Runner> runners =
-                List.of(new Runner("1", "박성우"), new Runner("2", "노준혁"), new Runner("3", "박현준"));
+        List<Runner> runners = List.of(박성우, 노준혁, 박현준);
 
         @Test
         @DisplayName("방을 생성한다.")
@@ -32,7 +34,7 @@ class BattleTest {
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
     class 러너의_인원이_올바르지_않을때 {
 
-        List<Runner> runners = List.of(new Runner("1", "박성우"), new Runner("2", "노준혁"));
+        List<Runner> runners = List.of(박성우, 노준혁);
 
         @Test
         @DisplayName("방을 생성한다.")
