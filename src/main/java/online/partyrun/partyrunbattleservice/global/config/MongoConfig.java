7e64747e6be880b10e9dfd8779dc061744ac9659
seongwoo <seongwoo0513@uos.ac.kr>
2023-06-29 11:10:04 +0900

    refactor : `@EnableMongoAuditing` 추가를 위한 MongoConfig 파일 추가 #PAR-112

---
+++ src/main/java/online/partyrun/partyrunbattleservice/global/config/MongoConfig.java
@@ -0,0 +1,11 @@
+package online.partyrun.partyrunbattleservice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoAuditing
+@EnableMongoRepositories("online.partyrun.partyrunbattleservice")
+public class MongoConfig {
+}
