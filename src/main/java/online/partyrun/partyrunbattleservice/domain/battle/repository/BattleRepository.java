483286137f8843b2e00189fa6e04dc6e5215026f
seongwoo <seongwoo0513@uos.ac.kr>
2023-06-29 14:17:05 +0900

    feat : 현재 진행중인 배틀을 찾는 메소드 추가 #PAR-118

--- src/main/java/online/partyrun/partyrunbattleservice/domain/battle/repository/BattleRepository.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/battle/repository/BattleRepository.java
@@ -7,7 +7,10 @@ import online.partyrun.partyrunbattleservice.domain.runner.entuty.Runner;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BattleRepository extends MongoRepository<Battle, String> {
     List<Battle> findByStatusAndRunnersIn(BattleStatus status, List<Runner> runners);
+
+    Optional<Battle> findByStatusAndRunnersId(BattleStatus status, String runnersId);
 }
--- src/test/java/online/partyrun/partyrunbattleservice/domain/battle/repository/BattleRepositoryTest.java
+++ src/test/java/online/partyrun/partyrunbattleservice/domain/battle/repository/BattleRepositoryTest.java
@@ -10,16 +10,29 @@ import online.partyrun.partyrunbattleservice.domain.runner.repository.RunnerRepo
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @DataMongoTest
 @DisplayName("BattleRepository")
 class BattleRepositoryTest {
 
-    @Autowired private RunnerRepository runnerRepository;
+    @Autowired
+    private RunnerRepository runnerRepository;
 
-    @Autowired private BattleRepository battleRepository;
+    @Autowired
+    private BattleRepository battleRepository;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @AfterEach
+    void tearDown() {
+        mongoTemplate.getDb().drop();
+    }
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -29,26 +42,56 @@ class BattleRepositoryTest {
         Runner 박현준 = runnerRepository.save(new Runner("박현준"));
         Runner 노준혁 = runnerRepository.save(new Runner("노준혁"));
 
-        @Test
-        @DisplayName("현재 참여중인 배틀이 없으면 빈 리스트를 반환한다.")
-        void returnEmpty() {
-            final List<Battle> actual =
-                    battleRepository.findByStatusAndRunnersIn(
-                            BattleStatus.RUNNING, List.of(박성우, 박현준, 노준혁));
-            assertThat(actual).isEmpty();
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class findByStatusAndRunnersIn은 {
+            @Test
+            @DisplayName("현재 참여중인 배틀이 없으면 빈 리스트를 반환한다.")
+            void returnEmpty() {
+                final List<Battle> actual =
+                        battleRepository.findByStatusAndRunnersIn(
+                                BattleStatus.RUNNING, List.of(박성우, 박현준, 노준혁));
+                assertThat(actual).isEmpty();
+            }
+
+            @Test
+            @DisplayName("현재 참여중인 배틀이 있으면 참여한 배틀 리스트를 반환한다.")
+            void returnListBattle() {
+                final Battle battle1 = battleRepository.save(new Battle(List.of(박성우, 박현준)));
+                final Battle battle2 = battleRepository.save(new Battle(List.of(노준혁)));
+
+                final List<Battle> actual =
+                        battleRepository.findByStatusAndRunnersIn(
+                                BattleStatus.RUNNING, List.of(박성우, 박현준, 노준혁));
+
+                assertThat(actual).usingRecursiveComparison().isEqualTo(List.of(battle1, battle2));
+            }
         }
 
-        @Test
-        @DisplayName("현재 참여중인 배틀이 있으면 참여한 배틀 리스트를 반환한다.")
-        void returnListBattle() {
-            final Battle battle1 = battleRepository.save(new Battle(List.of(박성우, 박현준)));
-            final Battle battle2 = battleRepository.save(new Battle(List.of(노준혁)));
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class findByStatusAndRunnersId는 {
+            Battle battle = battleRepository.save(new Battle(List.of(박성우, 박현준)));
 
-            final List<Battle> actual =
-                    battleRepository.findByStatusAndRunnersIn(
-                            BattleStatus.RUNNING, List.of(박성우, 박현준, 노준혁));
+            @Test
+            @DisplayName("현재 진행중인 상태의 배틀이 존재하면 반환한다.")
+            void returnRunningBattle() {
+                final Battle 박성우_진행중_배틀 = battleRepository.findByStatusAndRunnersId(BattleStatus.RUNNING, 박성우.getId()).orElseThrow();
+                final Battle 박현준_진행중_배틀 = battleRepository.findByStatusAndRunnersId(BattleStatus.RUNNING, 박현준.getId()).orElseThrow();
 
-            assertThat(actual).usingRecursiveComparison().isEqualTo(List.of(battle1, battle2));
+                assertThat(박성우_진행중_배틀)
+                        .usingRecursiveComparison()
+                        .isEqualTo(박현준_진행중_배틀)
+                        .isEqualTo(battle);
+            }
+
+            @Test
+            @DisplayName("현재 진행중인 상태의 배틀이 존재하지않으면 빈 값을 반환한다.")
+            void returnEmpty() {
+                final Optional<Battle> 노준혁_진행중_배틀 = battleRepository.findByStatusAndRunnersId(BattleStatus.RUNNING, 노준혁.getId());
+
+                assertThat(노준혁_진행중_배틀).isEmpty();
+            }
         }
     }
 }
