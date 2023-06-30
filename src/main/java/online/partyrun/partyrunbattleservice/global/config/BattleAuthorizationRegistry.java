39e9ea17c67d851c88a0bdaf52faaba36b659c1e
seong-wooo <seongwoo0513@uos.ac.kr>
2023-06-30 16:01:02 +0900

    feat : 사용되지 않는 주석 삭제 #PAR-118

--- src/main/java/online/partyrun/partyrunbattleservice/global/config/BattleAuthorizationRegistry.java
+++ src/main/java/online/partyrun/partyrunbattleservice/global/config/BattleAuthorizationRegistry.java
@@ -1,14 +1,10 @@
 package online.partyrun.partyrunbattleservice.global.config;
 
-import lombok.extern.slf4j.Slf4j;
-
 import online.partyrun.springsecurityauthorizationmanager.AuthorizationRegistry;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class BattleAuthorizationRegistry implements AuthorizationRegistry {
 
