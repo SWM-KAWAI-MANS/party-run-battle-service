e1a9735e2849d5d3924d0c9fb8f0f6527820c836
seongwoo <seongwoo0513@uos.ac.kr>
2023-06-30 12:17:52 +0900

    refactor : BattleControllerTest 문서 추가 #PAR-118

--- src/test/java/online/partyrun/partyrunbattleservice/domain/battle/controller/BattleControllerTest.java
+++ src/test/java/online/partyrun/partyrunbattleservice/domain/battle/controller/BattleControllerTest.java
@@ -1,15 +1,10 @@
 package online.partyrun.partyrunbattleservice.domain.battle.controller;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import online.partyrun.partyrunbattleservice.domain.battle.dto.BattleCreateRequest;
 import online.partyrun.partyrunbattleservice.domain.battle.dto.BattleResponse;
 import online.partyrun.partyrunbattleservice.domain.battle.exception.InvalidNumberOfBattleRunnerException;
 import online.partyrun.partyrunbattleservice.domain.battle.service.BattleService;
-import online.partyrun.testmanager.docs.RestControllerNoneAuthTest;
-
+import online.partyrun.testmanager.docs.RestControllerTest;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -22,10 +17,17 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Stream;
 
-@DisplayName("BattleRestController")
-class BattleControllerTest extends RestControllerNoneAuthTest {
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-    @MockBean BattleService battleService;
+@DisplayName("BattleRestController")
+class BattleControllerTest extends RestControllerTest {
+
+    @MockBean
+    BattleService battleService;
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -43,7 +45,7 @@ class BattleControllerTest extends RestControllerNoneAuthTest {
 
                 final ResultActions actions =
                         mockMvc.perform(
-                                post("/battle")
+                                post("/battle").with(csrf())
                                         .contentType(MediaType.APPLICATION_JSON)
                                         .characterEncoding(StandardCharsets.UTF_8)
                                         .content(toRequestBody(request)));
@@ -74,13 +76,40 @@ class BattleControllerTest extends RestControllerNoneAuthTest {
 
                 final ResultActions actions =
                         mockMvc.perform(
-                                post("/battle")
+                                post("/battle").with(csrf())
                                         .contentType(MediaType.APPLICATION_JSON)
                                         .characterEncoding(StandardCharsets.UTF_8)
                                         .content(toRequestBody(invalidRequest)));
                 actions.andExpect(status().isBadRequest());
 
                 setPrintDocs(actions, String.format("runner request is invalid, %s", message));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class 배틀_조회를_요청할_때 {
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 배틀에_참여하고_있는_러너의_id가_주어지면 {
+
+            @Test
+            @DisplayName("배틀 정보를 반환한다.")
+            void getRunningBattle() throws Exception {
+                given(battleService.getRunningBattle("박현준")).willReturn(new BattleResponse("battle_id"));
+
+                final ResultActions actions =
+                        mockMvc.perform(
+                                get("/battle/running")
+                                        .header("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .characterEncoding(StandardCharsets.UTF_8));
+
+                actions.andExpect(status().isOk());
+
+                setPrintDocs(actions, "get battle");
             }
         }
     }
