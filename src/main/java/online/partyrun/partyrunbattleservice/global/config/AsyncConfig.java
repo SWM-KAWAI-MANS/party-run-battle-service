a998c57b9e428af1bcaf55309b80a8d9032c0763
seong-wooo <seongwoo0513@uos.ac.kr>
2023-07-05 23:25:02 +0900

    feat : 비동기 처리에 관한 설정 추가 #PAR-130

---
+++ src/main/java/online/partyrun/partyrunbattleservice/global/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package online.partyrun.partyrunbattleservice.global.config;
+
+import online.partyrun.partyrunbattleservice.global.controller.AsyncExceptionHandler;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    private static final int THREAD_COUNT = 4;
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return Executors.newFixedThreadPool(THREAD_COUNT);
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
+}
