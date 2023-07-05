f55a400896ca2e574bd93a0ba94b423256230180
seong-wooo <seongwoo0513@uos.ac.kr>
2023-07-05 15:04:43 +0900

    feat : BattleTest 추가 #PAR-130

--- src/test/java/online/partyrun/partyrunbattleservice/domain/battle/entity/BattleTest.java
+++ src/test/java/online/partyrun/partyrunbattleservice/domain/battle/entity/BattleTest.java
@@ -3,6 +3,8 @@ package online.partyrun.partyrunbattleservice.domain.battle.entity;
 import online.partyrun.partyrunbattleservice.domain.battle.exception.InvalidDistanceException;
 import online.partyrun.partyrunbattleservice.domain.battle.exception.InvalidRunnerNumberInBattleException;
 import online.partyrun.partyrunbattleservice.domain.runner.entity.Runner;
+import online.partyrun.partyrunbattleservice.domain.runner.entity.RunnerStatus;
+import online.partyrun.partyrunbattleservice.domain.runner.exception.RunnerNotFoundException;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -10,8 +12,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("Battle")
 class BattleTest {
@@ -62,6 +63,40 @@ class BattleTest {
             void throwException(List<Runner> runners) {
                 assertThatThrownBy(() -> new Battle(distance, runners))
                         .isInstanceOf(InvalidRunnerNumberInBattleException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class 러너의_상태를_변경할_때 {
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 상태를_받으면 {
+
+            Battle 배틀 = new Battle(1000, List.of(박성우, 박현준));
+            RunnerStatus runnerStatus = RunnerStatus.RUNNING;
+
+            @Test
+            @DisplayName("러너의 상태를 변경한다.")
+            void changeRunnerStatus() {
+                배틀.changeRunnerStatus(박성우.getId(), runnerStatus);
+                assertThat(박성우.getStatus()).isEqualTo(runnerStatus);
+            }
+        }
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 배틀_내에_러너가_존재하지_않으면 {
+
+            Battle 배틀 = new Battle(1000, List.of(박성우, 박현준));
+
+            @Test
+            @DisplayName("예외를 던진다.")
+            void throwException() {
+                assertThatThrownBy(() -> 배틀.changeRunnerStatus(노준혁.getId(), RunnerStatus.RUNNING))
+                        .isInstanceOf(RunnerNotFoundException.class);
             }
         }
     }
