9fddef30f05bf91428a0e8c3bba4eb042dd6a61b
seong-wooo <seongwoo0513@uos.ac.kr>
2023-07-11 08:40:20 +0900

    refactor : fixture에 final 추가 #PAR-130

--- src/test/java/online/partyrun/partyrunbattleservice/fixture/MemberFixture.java
+++ src/test/java/online/partyrun/partyrunbattleservice/fixture/MemberFixture.java
@@ -4,9 +4,9 @@ import online.partyrun.partyrunbattleservice.domain.member.entity.Member;
 
 public class MemberFixture {
 
-    public static Member 멤버_박성우 = new Member("박성우");
-    public static Member 멤버_노준혁 = new Member("노준혁");
-    public static Member 멤버_박현준 = new Member("박현준");
-    public static Member 멤버_장세연 = new Member("장세연");
-    public static Member 멤버_이승열 = new Member("이승열");
+    public static final Member 멤버_박성우 = new Member("박성우");
+    public static final Member 멤버_노준혁 = new Member("노준혁");
+    public static final Member 멤버_박현준 = new Member("박현준");
+    public static final Member 멤버_장세연 = new Member("장세연");
+    public static final Member 멤버_이승열 = new Member("이승열");
 }
