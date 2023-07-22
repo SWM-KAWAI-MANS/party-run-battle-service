5c26fcfb113f7071ac21a994b576e60a565b0dfe
seong-wooo <seongwoo0513@uos.ac.kr>
2023-07-22 20:58:23 +0900

    feat : GpsDatas 테스트 추가

--- src/test/java/online/partyrun/partyrunbattleservice/domain/record/entity/GpsDatasTest.java
+++ src/test/java/online/partyrun/partyrunbattleservice/domain/record/entity/GpsDatasTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static online.partyrun.partyrunbattleservice.fixture.record.RecordFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -37,6 +38,42 @@ class GpsDatasTest {
             final GpsDatas gpsDatas = new GpsDatas(List.of(gpsData1, gpsData2, gpsData3));
 
             assertThat(gpsDatas.getGpsDatas()).usingRecursiveComparison().isEqualTo(List.of(gpsData3, gpsData2, gpsData1));
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class 기록을_생성할_때 {
+
+        @Test
+        @DisplayName("기존에 존재하던 기록을 통해 새로운 기록을 만든다.")
+        void createRecords() {
+
+            List<Record> actual = GPSDATAS_1.createRecords(RECORD_4);
+            double distance1 = GPSDATA_4.calculateDistance(GPSDATA_1);
+            double distance2 = GPSDATA_1.calculateDistance(GPSDATA_2);
+            double distance3 = GPSDATA_2.calculateDistance(GPSDATA_3);
+
+            Assertions.assertAll(
+                    () -> assertThat(actual).hasSize(3),
+                    () -> assertThat(actual.get(actual.size() - 1).getDistance())
+                            .isEqualTo(RECORD_4.getDistance() + distance1 + distance2 + distance3)
+            );
+        }
+
+        @Test
+        @DisplayName("기존에 존재하던 기록이 없을 때 새로운 기록을 만든다.")
+        void createNewRecords() {
+
+            List<Record> actual = GPSDATAS_1.createNewRecords();
+            double distance1 = GPSDATA_1.calculateDistance(GPSDATA_2);
+            double distance2 = GPSDATA_2.calculateDistance(GPSDATA_3);
+
+            Assertions.assertAll(
+                    () -> assertThat(actual).hasSize(3),
+                    () -> assertThat(actual.get(actual.size() - 1).getDistance())
+                            .isEqualTo(distance1 + distance2)
+            );
         }
     }
 }
