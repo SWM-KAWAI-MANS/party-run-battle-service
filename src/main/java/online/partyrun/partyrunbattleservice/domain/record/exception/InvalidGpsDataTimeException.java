84f3d7673614da387435a0530f2f8543f58188e4
seongwoo <seongwoo0513@uos.ac.kr>
2023-07-23 23:17:39 +0900

    refactor : 예외 메시지 변경

--- src/main/java/online/partyrun/partyrunbattleservice/domain/record/exception/InvalidGpsDataTimeException.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/record/exception/InvalidGpsDataTimeException.java
@@ -6,6 +6,6 @@ import java.time.LocalDateTime;
 
 public class InvalidGpsDataTimeException extends BadRequestException {
     public InvalidGpsDataTimeException(LocalDateTime time, LocalDateTime minTime) {
-        super(String.format("Gps 시간 %s 는 %s 보다 이후여야합니다.", time, minTime));
+        super(String.format("Gps 시간 %s 는 최소시간인 %s 보다 이후여야합니다.", time, minTime));
     }
 }
--- src/main/java/online/partyrun/partyrunbattleservice/domain/record/exception/PastGpsDataException.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/record/exception/PastGpsDataException.java
@@ -6,6 +6,6 @@ import java.time.LocalDateTime;
 
 public class PastGpsDataException extends BadRequestException {
     public PastGpsDataException(LocalDateTime time, LocalDateTime pastTime) {
-        super(String.format("%s 시간의 데이터는 과거 시간 %s 의 데이터", time, pastTime));
+        super(String.format("Gps %s 시간의 데이터는 과거 Gps 시간 %s 의 데이터보다 미래여야합니다.", time, pastTime));
     }
 }
