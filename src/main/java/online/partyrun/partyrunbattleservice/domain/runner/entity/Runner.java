a9740abbab3473567ed186e4ba5737a31d06adff
seong-wooo <seongwoo0513@uos.ac.kr>
2023-07-24 17:08:36 +0900

    refactor : RunnerRecord 삭제 및 Record를 Runner 내부로 옮김

--- src/main/java/online/partyrun/partyrunbattleservice/domain/runner/entity/Runner.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/runner/entity/Runner.java
@@ -4,10 +4,12 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-
+import online.partyrun.partyrunbattleservice.domain.record.entity.Record;
 import online.partyrun.partyrunbattleservice.domain.runner.exception.RunnerAlreadyFinishedException;
 import online.partyrun.partyrunbattleservice.domain.runner.exception.RunnerStatusCannotBeChangedException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Getter
@@ -16,10 +18,12 @@ import java.util.Objects;
 public class Runner {
     String id;
     RunnerStatus status;
+    List<Record> records;
 
     public Runner(String id) {
         this.id = id;
         this.status = RunnerStatus.READY;
+        this.records = new ArrayList<>();
     }
 
     public boolean hasId(String id) {
---
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/runner/entity/record/Location.java
@@ -0,0 +1,15 @@
+package online.partyrun.partyrunbattleservice.domain.runner.entity.record;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Location {
+    GeoJsonPoint point;
+    double altitude;
+}
