5c6358202e695752181bf33834856c469d3278fb
seong-wooo <seongwoo0513@uos.ac.kr>
2023-08-03 10:12:59 +0900

    refactor : 사용되지 않는 어노테이션 제거

--- src/main/java/online/partyrun/partyrunbattleservice/domain/battle/controller/BattleEventHandler.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/battle/controller/BattleEventHandler.java
@@ -3,19 +3,15 @@ package online.partyrun.partyrunbattleservice.domain.battle.controller;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
-
 import online.partyrun.partyrunbattleservice.domain.battle.dto.BattleWebSocketResponse;
 import online.partyrun.partyrunbattleservice.domain.battle.event.BattleRunningEvent;
 import online.partyrun.partyrunbattleservice.domain.battle.event.RunnerFinishedEvent;
 import online.partyrun.partyrunbattleservice.domain.battle.service.BattleService;
-
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 
-@Slf4j
 @Async
 @Controller
 @RequiredArgsConstructor
