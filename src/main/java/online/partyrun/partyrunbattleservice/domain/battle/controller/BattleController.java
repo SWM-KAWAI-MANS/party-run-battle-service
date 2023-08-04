866650a5ddc6e9504a3f46bb9db239dfd4441cda
seong-wooo <seongwoo0513@uos.ac.kr>
2023-08-04 12:52:59 +0900

    feat : 시각 확인하는 API 추가

--- src/main/java/online/partyrun/partyrunbattleservice/domain/battle/controller/BattleController.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/battle/controller/BattleController.java
@@ -1,18 +1,18 @@
 package online.partyrun.partyrunbattleservice.domain.battle.controller;
 
 import jakarta.validation.Valid;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-
 import online.partyrun.partyrunbattleservice.domain.battle.dto.BattleCreateRequest;
 import online.partyrun.partyrunbattleservice.domain.battle.dto.BattleResponse;
 import online.partyrun.partyrunbattleservice.domain.battle.service.BattleService;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class BattleController {
 
     BattleService battleService;
+    Clock clock;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -33,5 +34,11 @@ public class BattleController {
     @ResponseStatus(HttpStatus.OK)
     public BattleResponse getReadyBattle(Authentication auth) {
         return battleService.getReadyBattle(auth.getName());
+    }
+
+    @GetMapping("clock")
+    @ResponseStatus(HttpStatus.OK)
+    public LocalDateTime returnClock() {
+        return LocalDateTime.now(clock);
     }
 }
