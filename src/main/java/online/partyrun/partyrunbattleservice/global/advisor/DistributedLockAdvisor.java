5b7ae599c95560b8cf9068e1e31f1d58f073e8c1
seong-wooo <seongwoo0513@uos.ac.kr>
2023-08-06 14:21:29 +0900

    refactor: CustomerSpelParser 제거 및 DistributedLockAdvisor 로직 수정

--- src/main/java/online/partyrun/partyrunbattleservice/global/advisor/DistributedLockAdvisor.java
+++ src/main/java/online/partyrun/partyrunbattleservice/global/advisor/DistributedLockAdvisor.java
@@ -4,15 +4,16 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-
 import online.partyrun.partyrunbattleservice.global.annotation.DistributedLock;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -34,20 +35,11 @@ public class DistributedLockAdvisor {
         Method method = signature.getMethod();
         DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
 
-        String key =
-                REDISSON_LOCK_PREFIX
-                        + CustomSpelParser.getDynamicValue(
-                                signature.getParameterNames(),
-                                joinPoint.getArgs(),
-                                distributedLock.key());
+        String key = REDISSON_LOCK_PREFIX + getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributedLock.key());
         RLock rLock = redissonClient.getLock(key);
 
         try {
-            boolean available =
-                    rLock.tryLock(
-                            distributedLock.waitTime(),
-                            distributedLock.leaseTime(),
-                            distributedLock.timeUnit());
+            boolean available = rLock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
 
             if (!available) {
                 return false;
@@ -55,20 +47,20 @@ public class DistributedLockAdvisor {
 
             return joinPoint.proceed();
         } catch (InterruptedException e) {
-            throw new InterruptedException();
+            throw new InterruptedException("분산 락 처리 과정 중 에러가 발생했습니다.");
         } finally {
-            unLock(rLock, key, method);
+            rLock.unlock();
         }
     }
 
-    private void unLock(RLock rLock, String key, Method method) {
-        try {
-            rLock.unlock();
-        } catch (IllegalMonitorStateException e) {
-            log.info(
-                    String.format(
-                            "%s 메소드에 걸린 %s 키 Reddison Lock은 이미 unLock 상태입니다.",
-                            method.getName(), key));
+    public Object getDynamicValue(String[] parameterNames, Object[] args, String key) {
+        ExpressionParser parser = new SpelExpressionParser();
+        StandardEvaluationContext context = new StandardEvaluationContext();
+
+        for (int i = 0; i < parameterNames.length; i++) {
+            context.setVariable(parameterNames[i], args[i]);
         }
+
+        return parser.parseExpression(key).getValue(context, Object.class);
     }
 }
