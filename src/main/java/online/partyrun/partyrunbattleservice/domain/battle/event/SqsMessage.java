d30d71a0d8e39cefa30fc8b3c239068260fd995b
seongwoo <seongwoo0513@uos.ac.kr>
2023-08-10 16:21:04 +0900

    feat : 이벤트를 통해 Member 생성 로직 추가

---
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/battle/event/SqsMessage.java
@@ -0,0 +1,8 @@
+package online.partyrun.partyrunbattleservice.domain.battle.event;
+
+public record SqsMessage(String type, Object value) {
+    private static final String CREATED_EVENT_TYPE = "CREATED";
+    public boolean isCreatedMessage() {
+        return CREATED_EVENT_TYPE.equals(type);
+    }
+}
