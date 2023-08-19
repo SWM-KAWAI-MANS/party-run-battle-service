fe81914c9eb4301f10526b7697c080ee80c456a9
seong-wooo <seongwoo0513@uos.ac.kr>
2023-08-19 15:23:14 +0900

    refactor : 거리 측정 로직 변경

--- src/main/java/online/partyrun/partyrunbattleservice/domain/runner/entity/record/HaversineDistanceCalculator.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/runner/entity/record/HaversineDistanceCalculator.java
@@ -3,25 +3,32 @@ package online.partyrun.partyrunbattleservice.domain.runner.entity.record;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 
 public class HaversineDistanceCalculator implements DistanceCalculator<GeoJsonPoint, Double> {
-    private static final double EARTH_RADIUS = 6371;
+    private static final double EARTH_RADIUS = 6371009;
 
     @Override
     public Double calculate(GeoJsonPoint point1, GeoJsonPoint point2) {
-        final double lon1 = point1.getX();
         final double lat1 = point1.getY();
-        final double lon2 = point2.getX();
+        final double lng1 = point1.getX();
         final double lat2 = point2.getY();
+        final double lhg2 = point2.getX();
 
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lon2 - lon1);
+        return EARTH_RADIUS * distanceRadians(lat1, lng1, lat2, lhg2);
+    }
 
-        double a =
-                Math.sin(dLat / 2) * Math.sin(dLat / 2)
-                        + Math.cos(Math.toRadians(lat1))
-                                * Math.cos(Math.toRadians(lat2))
-                                * Math.sin(dLon / 2)
-                                * Math.sin(dLon / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return EARTH_RADIUS * c * 1000;
+    private double distanceRadians(double lat1, double lng1, double lat2, double lng2) {
+        return arcHav(havDistance(Math.toRadians(lat1), Math.toRadians(lat2), Math.toRadians(lng1) - Math.toRadians(lng2)));
+    }
+
+    private double havDistance(double lat1, double lat2, double dLng) {
+        return hav(lat1 - lat2) + hav(dLng) * Math.cos(lat1) * Math.cos(lat2);
+    }
+
+    private double hav(double x) {
+        double sinHalf = Math.sin(x * 0.5);
+        return sinHalf * sinHalf;
+    }
+
+    private double arcHav(double x) {
+        return 2 * Math.asin(Math.sqrt(x));
     }
 }
