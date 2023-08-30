999d43160179923aa01c3b7d17591f97c131a14f
seongwoo <seongwoo0513@uos.ac.kr>
2023-08-30 21:55:12 +0900

    fix : 러너의 상태를 변경하는 로직 버그 해결

--- src/test/java/online/partyrun/partyrunbattleservice/domain/battle/repository/BattleDaoTest.java
+++ src/test/java/online/partyrun/partyrunbattleservice/domain/battle/repository/BattleDaoTest.java
@@ -1,11 +1,8 @@
 package online.partyrun.partyrunbattleservice.domain.battle.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import online.partyrun.partyrunbattleservice.domain.battle.entity.Battle;
 import online.partyrun.partyrunbattleservice.domain.runner.entity.Runner;
 import online.partyrun.partyrunbattleservice.domain.runner.entity.RunnerStatus;
-
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,13 +10,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 @DisplayName("BattleDao")
 @SpringBootTest
 class BattleDaoTest {
 
-    @Autowired BattleDao battleDao;
+    @Autowired
+    BattleDao battleDao;
 
-    @Autowired BattleRepository battleRepository;
+    @Autowired
+    BattleRepository battleRepository;
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -27,9 +29,7 @@ class BattleDaoTest {
         Runner 박성우 = new Runner("박성우");
         Runner 박현준 = new Runner("박현준");
         Runner 노준혁 = new Runner("노준혁");
-        Battle 배틀 =
-                battleRepository.save(
-                        new Battle(1000, List.of(박성우, 박현준, 노준혁), LocalDateTime.now()));
+        Battle 배틀 = battleRepository.save(new Battle(1000, List.of(박성우, 박현준, 노준혁), LocalDateTime.now()));
 
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -38,10 +38,12 @@ class BattleDaoTest {
             @Test
             @DisplayName("배틀을 업데이트 후 반환한다.")
             void updateBattle() {
-                Battle battle =
-                        battleDao.updateRunnerStatus(배틀.getId(), 박성우.getId(), RunnerStatus.RUNNING);
-
-                assertThat(battle.getRunnerStatus(박성우.getId())).isEqualTo(RunnerStatus.RUNNING);
+                Battle battle = battleDao.updateRunnerStatus(배틀.getId(), 박현준.getId(), RunnerStatus.RUNNING);
+                assertAll(
+                        () -> assertThat(battle.getRunnerStatus(박성우.getId())).isEqualTo(RunnerStatus.READY),
+                        () -> assertThat(battle.getRunnerStatus(박현준.getId())).isEqualTo(RunnerStatus.RUNNING),
+                        () -> assertThat(battle.getRunnerStatus(노준혁.getId())).isEqualTo(RunnerStatus.READY)
+                );
             }
         }
     }
