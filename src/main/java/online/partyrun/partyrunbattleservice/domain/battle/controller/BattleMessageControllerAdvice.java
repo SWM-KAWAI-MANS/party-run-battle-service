066d147d1eebbcd628d7e128d7b0288b1d58e2a0
seongwoo <seongwoo0513@uos.ac.kr>
2023-09-06 21:32:35 +0900

    feat: 배틀 과정 중 예외 발생 시, 로깅 처리

--- src/main/java/online/partyrun/partyrunbattleservice/domain/battle/controller/BattleMessageControllerAdvice.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/battle/controller/BattleMessageControllerAdvice.java
@@ -1,19 +1,49 @@
 package online.partyrun.partyrunbattleservice.domain.battle.controller;
 
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-
+import online.partyrun.partyrunbattleservice.global.exception.BadRequestException;
+import online.partyrun.partyrunbattleservice.global.exception.NotFoundException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MissingRequestHeaderException;
+
+import java.util.stream.Collectors;
 
 @Slf4j
-@ControllerAdvice
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Controller
 public class BattleMessageControllerAdvice {
 
-    @MessageExceptionHandler
-    public void handleException(Exception exception) {
-        log.warn(exception.getMessage());
+    private static final String EXCEPTION_MESSAGE = "[EXCEPTION]";
+
+    @MessageExceptionHandler({
+            BadRequestException.class,
+            HttpMessageNotReadableException.class,
+            MissingRequestHeaderException.class
+    })
+    public void handleBadRequestException(Exception exception) {
+        log.warn("{} {}", EXCEPTION_MESSAGE, exception.getMessage());
+    }
+
+    @MessageExceptionHandler(BindException.class)
+    public void handleBindException(BindException exception) {
+        final String message =
+                exception.getBindingResult().getAllErrors().stream()
+                        .map(error -> String.format("%s: %s", ((FieldError) error).getField(), error.getDefaultMessage()))
+                        .collect(Collectors.joining(", "));
+
+        log.warn("{} {}",EXCEPTION_MESSAGE, message);
+    }
+
+    @MessageExceptionHandler(NotFoundException.class)
+    public void handleNotFoundException(NotFoundException exception) {
+        log.warn("{} {}",EXCEPTION_MESSAGE, exception.getMessage());
+    }
+
+    @MessageExceptionHandler({RuntimeException.class, Exception.class})
+    public void handleInternalServerErrorException(Exception exception) {
+        log.error("{} {}", EXCEPTION_MESSAGE, exception.getMessage());
     }
 }
