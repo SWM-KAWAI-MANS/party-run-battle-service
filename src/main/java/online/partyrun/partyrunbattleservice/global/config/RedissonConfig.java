a5fa53e57358b9aae67d2902c3f040fdad988132
박성우 <seongwoo0513@uos.ac.kr>
2023-09-12 09:20:53 +0900

    fix: aws redis cluster 연결 방식 변경 (#56)

--- src/main/java/online/partyrun/partyrunbattleservice/global/config/RedissonConfig.java
+++ src/main/java/online/partyrun/partyrunbattleservice/global/config/RedissonConfig.java
@@ -6,25 +6,11 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import java.util.List;
 
 @Configuration
 public class RedissonConfig {
 
     @Bean
-    @Profile("prd")
-    public RedissonClient clusterRedissonClient(@Value("${spring.data.redis.cluster.nodes}") List<String> nodes) {
-        Config config = new Config();
-        config.useClusterServers()
-                .addNodeAddress(nodes.toArray(new String[nodes.size()]));
-
-        return Redisson.create(config);
-    }
-
-    @Bean
-    @Profile("!prd")
     public RedissonClient singleRedissonClient(@Value("${spring.data.redis.url}") String redisUrl) {
         Config config = new Config();
         config.useSingleServer().setAddress(redisUrl);
