67c8fadf741a3f8ec814003f8d77f2e2cf946e9d
Hyeonjun Park <phjppo0918@gmail.com>
2023-09-13 16:08:17 +0900

    refactor : IllegalRunningTimeException 디렉터리 이동

--- src/main/java/online/partyrun/partyrunbattleservice/domain/single/entity/RunningTime.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/single/entity/RunningTime.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import online.partyrun.partyrunbattleservice.domain.single.exception.IllegalRunningTimeException;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
