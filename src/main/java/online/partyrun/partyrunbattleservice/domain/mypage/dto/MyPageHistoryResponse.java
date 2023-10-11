6e42243476f22ca95be733f0b5426d3928f9bace
seongwoo <seongwoo0513@uos.ac.kr>
2023-10-11 16:10:24 +0900

    fix: 기록이 없는 경우 빈 기록 반환

--- src/main/java/online/partyrun/partyrunbattleservice/domain/mypage/dto/MyPageHistoryResponse.java
+++ src/main/java/online/partyrun/partyrunbattleservice/domain/mypage/dto/MyPageHistoryResponse.java
@@ -3,11 +3,17 @@ package online.partyrun.partyrunbattleservice.domain.mypage.dto;
 import online.partyrun.partyrunbattleservice.domain.battle.entity.Battle;
 import online.partyrun.partyrunbattleservice.domain.single.entity.Single;
 
+import java.util.Collections;
 import java.util.List;
 
 public record MyPageHistoryResponse(List<MyPageRunningResponse> history) {
+    private static final MyPageHistoryResponse EMPTY_HISTORY = new MyPageHistoryResponse(Collections.emptyList());
 
     public static MyPageHistoryResponse ofBattles(List<Battle> battles, String memberId) {
+        if (battles.isEmpty()) {
+            return EMPTY_HISTORY;
+        }
+
         return new MyPageHistoryResponse(
                 battles.stream()
                         .map(battle -> MyPageRunningResponse.of(battle, memberId))
@@ -16,6 +22,9 @@ public record MyPageHistoryResponse(List<MyPageRunningResponse> history) {
     }
 
     public static MyPageHistoryResponse ofSingle(List<Single> singles) {
+        if (singles.isEmpty()) {
+            return EMPTY_HISTORY;
+        }
         return new MyPageHistoryResponse(
                 singles.stream()
                         .map(MyPageRunningResponse::from)
